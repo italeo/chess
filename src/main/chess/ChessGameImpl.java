8f1e1ea90dc4f03cf07d9dbc74a0575a6e857ba3
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-10-06 15:18:49 -0600

    ChessGame, (worked on) validMoves, makeMoves, stBoard, getBoard.

--- src/main/chess/ChessGameImpl.java
+++ src/main/chess/ChessGameImpl.java
@@ -1,27 +1,56 @@
 package chess;
 
-import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class ChessGameImpl implements ChessGame {
+
+    private TeamColor teamTurn;
+    private ChessBoard board = new ChessBoardImpl();
+
+    public ChessGameImpl() {
+        this.teamTurn = TeamColor.WHITE;
+    }
+
     @Override
     public TeamColor getTeamTurn() {
-        return null;
+        return teamTurn;
     }
 
     @Override
     public void setTeamTurn(TeamColor team) {
+        teamTurn = team;
 
     }
 
     @Override
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        return null;
+        List<ChessMove> finalMoves = new ArrayList<>();
+        ChessPiece piece = getBoard().getPiece(startPosition);
+        List<ChessMove> allMoves = (List<ChessMove>) piece.pieceMoves(board, startPosition);
+        for (int i = 0; i < allMoves.size(); i++) {
+            if (!isInCheck(teamTurn)) {
+                finalMoves.add(allMoves.get(i));
+            }
+        }
+        return finalMoves;
     }
 
     @Override
     public void makeMove(ChessMove move) throws InvalidMoveException {
+        List<ChessMove> moves = (List<ChessMove>) validMoves(move.getStartPosition());
+        if (moves.contains(move)) {
+            ChessPiece piece = getBoard().getPiece(move.getStartPosition());
+            board.addPiece(move.getEndPosition(), piece);
+            // FUNCTION NEEDED FOR removePiece()
+
+        }
+        else {
+            throw  new InvalidMoveException();
+        }
+
 
     }
 
@@ -42,11 +71,12 @@ public class ChessGameImpl implements ChessGame {
 
     @Override
     public void setBoard(ChessBoard board) {
+        this.board = board;
 
     }
 
     @Override
     public ChessBoard getBoard() {
-        return null;
+        return board;
     }
 }
