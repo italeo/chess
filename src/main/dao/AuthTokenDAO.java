b517959fe3a13f11a6d3e91cea34792f5fed02fa
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-10-12 14:20:47 -0600

    ChessGame, (worked on) phase 2, stubbed out DAO for authToken and model authToken class

--- src/main/dao/AuthTokenDAO.java
+++ src/main/dao/AuthTokenDAO.java
@@ -1,4 +1,26 @@
 package dao;
 
+import model.AuthToken;
+
+import java.sql.Connection;
+
+/** Responsible for accessing the data for the AuthToken */
 public class AuthTokenDAO {
+    /** Creates the connection between the server and the database. */
+
+    private final Connection conn;
+
+    /** Constructs the connection between the server and the database to access the information needed for the authToken
+     * @param conn - associated connection for data access.
+     * */
+    public AuthTokenDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    /** This function will insert an authToken for a user creates a new account or logs in.
+     * @param authToken - The unique token used to represent the user.
+     * @throws DatabaseException - thrown when there is a database error.
+     * */
+    public void insert(AuthToken authToken) throws DatabaseException {
+    }
 }
--- src/main/model/AuthToken.java
+++ src/main/model/AuthToken.java
@@ -1,4 +1,44 @@
 package model;
 
+import java.util.Objects;
+
+/** Sets the structure for the AuthToken table in the database*/
 public class AuthToken {
+    /** The authorization token of a signed-in user. */
+    private String authToken;
+    /** The user's username, used by other players to identify the user. */
+    private String username;
+
+    /** Constructs the authToken object the user gets when logged into the game
+     * @param authToken - The unique token given to the user.
+     * @param username - The username associate with the user.
+     * */
+    public AuthToken(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /** Compares the authToken from the database to see if we have the right authToken.
+     * @param obj - The authToken returned from the database.
+     * */
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
 }
