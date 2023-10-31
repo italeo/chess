1416cbd2864c7cf252ed4baae73e6d64216f7ac4
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-10-31 16:13:48 -0600

    ChessGame, (worked on) start phase-4, completed UserDAO - fixed db to connection

--- src/main/dao/AuthTokenDAO.java
+++ src/main/dao/AuthTokenDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 /** Responsible for accessing the data for the AuthToken */
 public class AuthTokenDAO {
-    private Database db;
+    private Connection conn;
 
     public AuthTokenDAO() {
     }
@@ -18,8 +18,8 @@ public class AuthTokenDAO {
      //* @param conn - associated connection for data access.
      * */
 
-    public AuthTokenDAO(Database db) {
-        this.db = db;
+    public AuthTokenDAO(Connection conn) {
+        this.conn = conn;
     }
 
     /** This function will insert an authToken for a user creates a new account or logs in.
@@ -27,16 +27,16 @@ public class AuthTokenDAO {
      * @throws DataAccessException - thrown when there is a database error.
      * */
     public void insert(AuthToken authToken) throws DataAccessException {
-        try(Connection conn = db.getConnection()) {
-            String sql = "INSERT INTO AuthToken (authToken, username) VALUES (?, ?)";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, authToken.getAuthToken());
-                stmt.setString(2, authToken.getUsername());
-                stmt.executeUpdate();
-            }
-        } catch (SQLException | dataAccess.DataAccessException e) {
+
+        String sql = "INSERT INTO AuthToken (authToken, username) VALUES (?, ?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, authToken.getAuthToken());
+            stmt.setString(2, authToken.getUsername());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
             throw new DataAccessException("Error while inserting into AuthToken table: " + e.getMessage());
         }
+
     }
 
     /** This clear function removes the authorization token from the database that belongs to the user.
@@ -44,45 +44,39 @@ public class AuthTokenDAO {
      * @throws DataAccessException - thrown when there is a database error.
      * */
     public void clear() throws DataAccessException {
-        try (Connection conn = db.getConnection()) {
-            String sql = "DELETE FROM AuthToken";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.executeUpdate();
-            }
-        } catch (SQLException | dataAccess.DataAccessException e) {
+        String sql = "DELETE FROM AuthToken";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
             throw new DataAccessException("Error while try to clear AuthToken table: " + e.getMessage());
         }
-
     }
 
     /** This function allows us to query the  right table in the database for the specific authorization token specified by:
      * @param authToken - The specific authToken we are trying to find.
      * @throws DataAccessException - thrown when there is a database error.*/
     public AuthToken find(String authToken) throws DataAccessException {
-        try (Connection conn = db.getConnection()) {
-            String sql = "SELECT * FROM AuthToken WHERE authToken = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, authToken);
-                try (ResultSet rs = stmt.executeQuery()) {
-                    if (rs.next()) {
-                        return new AuthToken(rs.getString("authToken"), rs.getString("username"));
-                    }
+
+        String sql = "SELECT * FROM AuthToken WHERE authToken = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, authToken);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthToken(rs.getString("authToken"), rs.getString("username"));
                 }
             }
-        } catch (SQLException | dataAccess.DataAccessException e) {
+        } catch (SQLException e) {
             throw new DataAccessException("Error while querying the AuthToken table: " + e.getMessage());
         }
         return null;
     }
 
     public void delete(String authToken) throws DataAccessException {
-        try (Connection conn = db.getConnection()) {
-            String sql = "DELETE FROM AuthToken WHERE authToken = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, authToken);
-              stmt.executeUpdate();
-            }
-        } catch (SQLException | dataAccess.DataAccessException e) {
+        String sql = "DELETE FROM AuthToken WHERE authToken = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, authToken);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
             throw new DataAccessException("Error with deleting an authToken from AuthToken table: " + e.getMessage());
         }
     }
