baa07d5016bdb3001f86098c32fa957cb0114a34
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-10-31 09:43:53 -0600

    ChessGame, (worked on) start phase-4, started the AuthTokenDAO

--- src/main/dao/AuthTokenDAO.java
+++ src/main/dao/AuthTokenDAO.java
@@ -1,18 +1,25 @@
 package dao;
 
+import dataAccess.Database;
 import model.AuthToken;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 /** Responsible for accessing the data for the AuthToken */
 public class AuthTokenDAO {
-    private static Map<String, AuthToken> authTokenMap = new HashMap<>();
+    private final Database db;
 
     /** Constructs the connection between the Server and the database to access the information needed for the authToken
      //* @param conn - associated connection for data access.
      * */
-    public AuthTokenDAO() {
-        //this.authTokenMap = new HashMap<>();
+
+
+    public AuthTokenDAO(Database db) {
+        this.db = db;
     }
 
     /** This function will insert an authToken for a user creates a new account or logs in.
@@ -20,7 +27,16 @@ public class AuthTokenDAO {
      * @throws DataAccessException - thrown when there is a database error.
      * */
     public void insert(AuthToken authToken) throws DataAccessException {
-        authTokenMap.put(authToken.getAuthToken(), authToken);
+        try(Connection conn = db.getConnection()) {
+            String sql = "INSERT INTO AuthToken (token, username) VALUES (?, ?)";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, authToken.getAuthToken());
+                stmt.setString(2, authToken.getUsername());
+                stmt.executeUpdate();
+            }
+        } catch (SQLException | dataAccess.DataAccessException e) {
+            throw new DataAccessException("Error while inserting authToken: " + e.getMessage());
+        }
     }
 
     /** This clear function removes the authorization token from the database that belongs to the user.
@@ -28,18 +44,26 @@ public class AuthTokenDAO {
      * @throws DataAccessException - thrown when there is a database error.
      * */
     public void clear() throws DataAccessException {
-        authTokenMap.clear();
+        try (Connection conn = db.getConnection()) {
+            String sql = "DELETE FROM AuthToken";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            }
+        } catch (SQLException | dataAccess.DataAccessException e) {
+            throw new DataAccessException("Error while try to clear AuthToken table: " + e.getMessage());
+        }
+
     }
 
     /** This function allows us to query the  right table in the database for the specific authorization token specified by:
      * @param authToken - The specific authToken we are trying to find.
      * @throws DataAccessException - thrown when there is a database error.*/
     public AuthToken find(String authToken) throws DataAccessException {
-        return authTokenMap.get(authToken);
+        return null;
     }
 
     // Modified this function from phase-2 to properly clear the hashMap
     public void delete(String token) throws DataAccessException {
-        authTokenMap.remove(token);
+
     }
 }
