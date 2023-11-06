0e7c492d4388e19ed2bd6ec8a66d82f2108b8806
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-11-06 14:00:24 -0700

    ChessGame, (worked on) junit services test cases working on userDAO test

--- src/test/passoffTests/DAOTests/userDAOTests.java
+++ src/test/passoffTests/DAOTests/userDAOTests.java
@@ -56,8 +56,57 @@ public class userDAOTests {
     public void insertTest_Fail() throws DataAccessException {
         // For this test we are checking that situation of inserting the same user twice, and an invalid user.
         User testUser2 = testUser;
-
         userDAO.insert(testUser);
+
+        // Testing to see if we pass in the same username again, it is handled correctly
         assertThrows(DataAccessException.class, ()-> userDAO.insert(testUser2));
+    }
+
+    @Test
+    public void clearTest_Success() throws DataAccessException {
+
+        // Add our test user into the db
+        userDAO.insert(testUser);
+
+        // Verify that the table is not empty and contains our test user
+        assertNotNull(userDAO.find("italeo"));
+
+        // Call the clear method
+        assertDoesNotThrow(() -> userDAO.clear());
+
+        // Check that the db is actually clear
+        assertNull(userDAO.find("italeo"));
+    }
+
+    @Test
+    public void findTest_Success() throws DataAccessException {
+        // Insert the test user into the db to test the find method
+        userDAO.insert(testUser);
+
+        // Test the search method
+        User test = userDAO.find("italeo");
+
+        // Check if the db is not null
+        assertNotNull(test);
+    }
+
+    @Test
+    public void findTest_fail() throws DataAccessException {
+
+        // Creating a user that does not exist in the db
+        User nonExistingUser = null;
+
+        // Add the test user into the db
+        userDAO.insert(testUser);
+
+        // Try to find the nonExistingUser from the db
+        try {
+            nonExistingUser = userDAO.find("Ishmael");
+        } catch (DataAccessException e) {
+            fail("Exception should not be thrown");
+        }
+
+        // Check that the retrieved user is null
+        assertNull(nonExistingUser);
     }
 }
