b2bf544029d8f18acff5e5f1763b392309a4f688
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-11-10 11:09:45 -0700

    ChessGame, (worked on), STARTING phase-5 - fixed minor changes

--- client/src/main/ui/ChessClient.java
+++ client/src/main/ui/ChessClient.java
@@ -5,15 +5,20 @@ import ui.websocket.NotificationHandler;
 public class ChessClient {
     public final String serverUrl;
     private final NotificationHandler notificationHandler;
-    private State state = State.SIGNEDOUT;
+    private State state = State.LOGGED_OUT;
 
     public ChessClient(String serverUrl, NotificationHandler notificationHandler) {
         this.serverUrl = serverUrl;
         this.notificationHandler = notificationHandler;
     }
 
+    // Need to complete!!
+    public String eval(String input) {
+        return "";
+    }
+
     public String help() {
-        if (state == State.SIGNEDOUT) {
+        if (state == State.LOGGED_OUT) {
             return """
                     register <USERNAME> <PASSWORD> <EMAIL> - to create an account
                     login <USERNAME> <PASSWORD> - to play chess
--- client/src/main/ui/Repl.java
+++ client/src/main/ui/Repl.java
@@ -2,7 +2,10 @@ package ui;
 
 import ui.websocket.NotificationHandler;
 import javax.management.Notification;
+import java.util.Scanner;
 
+import static ui.EscapeSequences.*;
+import static ui.State.*;
 public class Repl implements NotificationHandler {
     // Needed to call the client that handles all the functionality of the game
     private final ChessClient client;
@@ -14,11 +17,25 @@ public class Repl implements NotificationHandler {
     public void run() {
         System.out.println("👑 Welcome to 240 chess. Type Help to get started. 👑");
         System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+//            try {
+//                result = client.eval(line);
+//            } catch ()
+        }
     }
 
 
     @Override
     public void notify(Notification notification) {
+    }
 
+    private void printPrompt() {
+        System.out.print("\n" +  SET_TEXT_BOLD + ">>>" + SET_TEXT_COLOR_GREEN);
     }
 }
