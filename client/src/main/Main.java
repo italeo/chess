18da8b7bdf2b21f28fe764e0e41af9c6dbd8b38f
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-11-10 08:28:40 -0700

    ChessGame, (worked on), STARTING phase-5 - Created a main class, trying to get it to work

---
+++ client/src/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var serverURL = "http://localhost:8080";
+        if (args.length == 1) {
+            serverURL = args[0];
+        }
+        System.out.println("Hello, World!");
+    }
+}
