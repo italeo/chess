d67200c6f08305c7a80378aa31616db1b232eae0
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-11-21 13:48:49 -0700

    ChessGame, (worked on), phase-5 - fixed just the board

--- client/src/main/ui/ChessBoardDrawer.java
+++ client/src/main/ui/ChessBoardDrawer.java
@@ -3,10 +3,9 @@ import chess.*;
 
 public class ChessBoardDrawer {
 
-    private static final int SQUARE_SIZE_IN_CHARS = 3;
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final String WHITE_SQUARE = EscapeSequences.SET_BG_COLOR_WHITE + " ";
-    private static final String BLACK_SQUARE = EscapeSequences.SET_BG_COLOR_BLACK + " ";
+    private static final String WHITE_SQUARE = EscapeSequences.SET_BG_COLOR_WHITE;
+    private static final String BLACK_SQUARE = EscapeSequences.SET_BG_COLOR_BLACK;
     private static final String BORDER_COLOR = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
 
 
@@ -37,24 +36,10 @@ public class ChessBoardDrawer {
 
     private void drawChessSquare(int row, int col) {
         if ((row + col) % 2 == 0) {
-            setWhite();
             System.out.print(WHITE_SQUARE);
         } else {
-            setBlack();
             System.out.print(BLACK_SQUARE);
         }
-
-        if (col < BOARD_SIZE_IN_SQUARES) {
-            setBlack();
-            System.out.print(EscapeSequences.EMPTY);
-        }
-    }
-
-    private void setBlack() {
-        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
-    }
-
-    private void setWhite() {
-        System.out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
+        System.out.print(EscapeSequences.EMPTY);
     }
 }
