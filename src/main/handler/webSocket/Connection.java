a5a66b88c13b994d33c692731b503548252de444
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-12-02 11:04:28 -0700

    ChessGame, (worked on), phase-6. Added connection

--- src/main/handler/webSocket/Connection.java
+++ src/main/handler/webSocket/Connection.java
@@ -1,4 +1,18 @@
 package handler.webSocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import java.io.IOException;
+
 public class Connection {
+    public int gameIndex;
+    public Session session;
+
+    public Connection(int gameIndex, Session session) {
+        this.gameIndex = gameIndex;
+        this.session = session;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
 }
