5bad062553bec151ac19985ea1e91a0c073b43e7
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-12-04 08:29:45 -0700

    ChessGame, (worked on), phase-6. created connections variable in webSockectHandler

--- src/main/handler/webSocket/WebSocketHandler.java
+++ src/main/handler/webSocket/WebSocketHandler.java
@@ -1,4 +1,12 @@
 package handler.webSocket;
 
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
 public class WebSocketHandler {
+
+    // Responsible to handle all web socket connections
+    private final ConnectionManager connections = new ConnectionManager();
+
+
 }
