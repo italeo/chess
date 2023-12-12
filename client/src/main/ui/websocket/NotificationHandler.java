205e030a2a7684419a86e70d578849d44bcf1c43
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-12-12 14:55:45 -0700

    ChessGame, (worked on), phase-6. Starting the client GAME_PLAY

--- client/src/main/ui/websocket/NotificationHandler.java
+++
@@ -1,7 +0,0 @@
-package ui.websocket;
-
-import javax.management.Notification;
-
-public interface NotificationHandler {
-    void notify(Notification notification);
-}
--- shared/src/main/exception/ResponseException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ResponseException extends Exception {
-    public ResponseException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}
